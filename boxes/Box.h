7700489f183a840bb24400819482da0c4377f78c
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-08-05 14:28:01 -0400

    First commit - simple division / volume calculating command line example

---
+++ boxes/Box.h
@@ -0,0 +1,13 @@
+//
+//  Box.h
+//  boxes
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-08-05.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+
+@end
---
+++ boxes/Box.m
@@ -0,0 +1,13 @@
+//
+//  Box.m
+//  boxes
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-08-05.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+@end
